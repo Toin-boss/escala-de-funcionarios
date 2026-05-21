c597aff6814d1744b3035ae272c38762ff43cefd
Antônio Fernandes <antfsn12@gmail.com>
2026-05-20 22:19:57 -0300

    Correção

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Olá, mundo, sou eu de novo");
-        System.out.println("Olá mais uma vez")
+        System.out.println("Olá mais uma vez");
     }
 }
